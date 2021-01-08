fddbb6d8ba38e2766e671da92228d9618e734f4c
Austin W <austinwoo123@gmail.com>
2021-01-07 19:24:39 -0800

    define variables and create a variable to store teh searched city

---
+++ script.js
@@ -0,0 +1,36 @@
+// search-city
+// search-button
+// selected-weather
+// current-city
+// temp
+// humidity
+// wind
+// uv
+// weather-future
+
+// assign variables// 
+var searchCity = $("#search-city");
+var searchButto = $("#search-button");
+var selectedWeather = $("#selected-weather");
+var currentCity = $("#current-city");
+var currentTemp = $("#temp");
+var currentHumidity = $("#humidity");
+var currentWind = $("#wind");
+var weatherFuture = $("#weather-future");
+
+var city = "";
+
+var Cities = [];
+
+
+// API Key goes here //
+var APIkey = "259210c99a8f0d57388d317f98069395";
+
+
+
+
+
+
+
+
+
